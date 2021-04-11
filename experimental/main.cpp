5b33766011b83681ac65412371898ab7880bd6d9
Rami-J <rami2162@hotmail.com>
2021-04-11 00:18:10 -0400

    Add performance analysis Qt project files

---
+++ experimental/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// TODO: decode sample audio files in the experimental/ directory using
+// QAudioDecoder and pass the data into all 4 transform algorithms. Time each
+// function using std::chrono over a set number of trials.
+int main(int argc, char *argv[])
+{
+	std::cout << "TODO" << std::endl;
+	return 0;
+}
+ 
